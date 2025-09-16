2e9707bac1f553043f83ffd1a1eaa06e3d27a14c
olmsteadca <olmsteadca@vcu.edu>
2025-09-15 22:33:10 -0400

    one last timeee

--- src/components/contactFooter/ContactFooter.jsx
+++ src/components/contactFooter/ContactFooter.jsx
@@ -10,19 +10,19 @@ function ContactFooter() {
         <div className="contact-row">
 
           <a href="mailto:celesteolmstead10@gmail.com">
-            <img src="/icons/email.svg" className="contact-icon" />
+            <img src="./icons/email.svg" className="contact-icon" />
           </a>
 
           <a href="https://www.figma.com/@celesteolmstead" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/figma.svg" className="contact-icon" />
+            <img src="./icons/figma.svg" className="contact-icon" />
           </a>
 
           <a href="https://www.linkedin.com/in/celesteolmstead0921" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/linkedin.svg" className="contact-icon" />
+            <img src="./icons/linkedin.svg" className="contact-icon" />
           </a>
 
           {/* on click, little alert comes up - could use later as a security measure by having to type each digit to reveal the next so i dont get scam calls */}
-          <img src="/icons/phone.svg" className="contact-icon" />
+          <img src="./icons/phone.svg" className="contact-icon" />
 
         </div>
       </div>
